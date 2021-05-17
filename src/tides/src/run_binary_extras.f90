4bc69c057569c18fca1de6c372e44ef2ae3dca91
Adolfo Simaz Bunzel <asimazbunzel@gmail.com>
2021-05-17 16:26:01 -0300

    add skeleton for tides in non-coplanar case

--- src/tides/src/run_binary_extras.f90
+++ src/tides/src/run_binary_extras.f90
@@ -31,6 +31,8 @@
       use math_lib
       
       implicit none
+
+      logical, parameter :: dbg = .true.
       
       contains
       
@@ -45,6 +47,10 @@
             write(*,*) 'failed in binary_ptr'
             return
          end if
+
+         ! apply tides in non-coplanar case
+         b% other_sync_spin_to_orbit => sync_non_coplanar
+         b% other_edot_tidal => edot_non_coplanar
 
          ! Set these function pointers to point to the functions you wish to use in
          ! your run_binary_extras. Any which are not set, default to a null_ version
@@ -65,6 +71,53 @@
           b% warn_binary_extra =.false.
          
       end subroutine extras_binary_controls
+
+
+      subroutine sync_non_coplanar(id, nz, osep, qratio, rl, dt_next, Ftid, sync_type, sync_mode, ierr)
+         integer, intent(in) :: id
+         integer, intent(in) :: nz
+         real(dp), intent(in) :: osep ! orbital separation (cm)
+         real(dp), intent(in) :: qratio ! mass_other_star/mass_this_star
+         real(dp), intent(in) :: rl ! roche lobe radius (cm)
+         real(dp), intent(in) :: dt_next ! next timestep
+         real(dp), intent(in) :: Ftid ! efficiency of tidal synchronization. (time scale / Ftid)
+         character (len=strlen), intent(in) :: sync_type ! synchronization timescale
+         character (len=strlen), intent(in) :: sync_mode ! where to put/take angular momentum
+         integer, intent(out) :: ierr
+         type(star_info), pointer :: s
+         integer :: k
+
+         call star_ptr(id, s, ierr)
+         if (ierr /= 0) then
+            write(*,*) 'failed in star_ptr'
+            return
+         end if
+
+         if (dbg) write(*,*) 'going to compute torque due to tides on a non-coplanar case'
+
+         do k=1, nz
+            s% extra_jdot(k) = s% extra_jdot(k) - 0d0
+         end do
+
+      end subroutine sync_non_coplanar
+
+      subroutine edot_non_coplanar(binary_id, ierr)
+         integer, intent(in) :: binary_id
+         integer, intent(out) :: ierr
+         type(binary_info), pointer :: b
+
+         ierr = 0
+         call binary_ptr(binary_id, b, ierr)
+         if (ierr /= 0) then
+            write(*,*) 'failed in binary_ptr'
+            return
+         end if
+         
+         if (dbg) write(*,*) 'going to compute eccentricity change on a non-coplanar case'
+
+         b% edot_tidal = 0d0
+
+      end subroutine edot_non_coplanar
 
 
       integer function how_many_extra_binary_history_header_items(binary_id)
